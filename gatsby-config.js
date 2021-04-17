3d3e4bd5b9aed5fdef11c0869704c8306ae739bd
Aaron Cox <i.aaroncox@gmail.com>
2021-04-16 22:30:01 -0400

    add icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,34 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icons/android-chrome-512x512.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `src/images/icons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/icons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/icons/apple-touch-icon.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/icons/favicon-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/icons/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+        ]
       },
     },
     `gatsby-plugin-gatsby-cloud`,
