71d55e4c6e2d114709b3ca9bb0f134c23e132fbc
Aaron Cox <i.aaroncox@gmail.com>
2021-12-12 12:42:34 -0600

    nothing too awkward in spacing anymore in showcase

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -4,36 +4,38 @@ import { StaticImage} from "gatsby-plugin-image";
 export function Showcase() {
   return (
     <div className="has-background-dark p-6">
-      <div className="container">
+      <div className="container p-6">
+        <div className="columns">
 
-            <div className="has-background-dark">
-              <h1 className="title is-1 has-text-info has-permanent-marker-font pb-1">Hello!</h1>
-              <h3 className="subtitle is-3 has-text-light has-text-weight-bold pb-3">I'm Aaron and I develop software.</h3>
-              <p className="block has-text-light is-size-5">
-                I currently do it for&nbsp;<a href="https://www.beckman.com" target="_blank" className="has-text-danger">Beckman Coulter</a>&nbsp;
-                in Indianapolis, Indiana. The mission of the position is to integrate medical research devices with our liquid-handling
-                robotic automation platforms. I also get to spend a significant chunk of my time working with modern web development
-                projects, mostly with modern technologies.
-              </p>
-              <p className="block has-text-light is-size-5">
-                I attended&nbsp;<a href="https://www.indstate.edu" target="_blank" className="has-text-primary">Indiana State University</a>&nbsp;
-                in Terre Haute, Indiana. I studied computer science and received M.S. and B.S. degrees for my efforts.
-                I gravitated to multidisciplinary work, participating in bioinformatics and physics projects.
-              </p>
-            </div>
+          <div className="column is-narrow-desktop">
+            <figure className="image">
+              <StaticImage
+                src="../images/me-jenga-square.jpg"
+                alt="Me, in my backyard, learning on a giant Jenga set, grinning like a fool."
+                className="has-ratio is-centered"
+                style={{borderRadius: "1rem", maxWidth: "500px"}}
+              />
+            </figure>
+          </div>
 
-            <div className="mt-6">
-              <figure className="image">
-                <StaticImage
-                  src="../images/me-jenga-square.jpg"
-                  alt="Me, in my backyard, learning on a giant Jenga set, grinning like a fool."
-                  className="has-ratio is-centered"
-                  style={{borderRadius: "1rem"}}
-                />
-              </figure>
-            </div>
+          <div className="column">
+            <h1 className="title is-1 has-text-info has-permanent-marker-font pb-1">Hello!</h1>
+            <h3 className="subtitle is-3 has-text-light has-text-weight-bold pb-3">I'm Aaron and I develop software.</h3>
+            <p className="block has-text-light is-size-5">
+              I currently do it for&nbsp;<a href="https://www.beckman.com" target="_blank" className="has-text-danger">Beckman Coulter</a>&nbsp;
+              in Indianapolis, Indiana. The mission of the position is to integrate medical research devices with our liquid-handling
+              robotic automation platforms. I also get to spend a significant chunk of my time working with modern web development
+              projects, mostly with modern technologies.
+            </p>
+            <p className="block has-text-light is-size-5">
+              I attended&nbsp;<a href="https://www.indstate.edu" target="_blank" className="has-text-primary">Indiana State University</a>&nbsp;
+              in Terre Haute, Indiana. I studied computer science and received M.S. and B.S. degrees for my efforts.
+              I gravitated to multidisciplinary work, participating in bioinformatics and physics projects.
+            </p>
+          </div>
 
 
+        </div>
       </div>
     </div>
   );
